19fe743a80053f9846b2edac9528e904ee262e24
Quy Hoang <quyhbn@nal.vn>
2019-08-23 18:35:09 +0700

    docker and config

---
+++ containers/wpcli/install_packages.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+wp plugin install contact-form-7 --activate --allow-root
+wp plugin install advanced-custom-fields --activate --allow-root
+wp plugin install smart-slider-3 --activate --allow-root
+wp plugin install cpt-bootstrap-carousel --activate --allow-root
